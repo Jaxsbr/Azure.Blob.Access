a0133d099e564c1301feb9002bdc8d41283d70a7
Jaco Brink <Jaco.Brink@derivco.co.za>
2018-09-14 08:29:15 +0200

    Controller wiring and other changes

---
+++ src/Azure.BlobAccess.API/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.BlobAccess.API.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IActionResult ReturnActionResult<T>(T data)
+        {
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Json(data);
+        }
+    }
+}
---
+++ src/Azure.BlobAccess.API/Controllers/BlobContainerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.BlobAccess.Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.BlobAccess.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobContainerController : BaseController
+  {
+        private readonly IBlobContainerRepository _blobContainerRepository;          
+
+        public BlobContainerController(IBlobContainerRepository blobContainerRepository)
+        {
+            _blobContainerRepository = blobContainerRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return ReturnActionResult(await _blobContainerRepository.GetAllBlobContainers());
+        }
+
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            return ReturnActionResult(await _blobContainerRepository.GetBlobContainerByName(name));
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
