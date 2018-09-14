9afc82c6120ba00cbc46a2ca26b677bc1f329d33
Jaco Brink <Jaco.Brink@derivco.co.za>
2018-09-14 07:38:45 +0200

    Added core project and some interfaces

---
+++ src/Azure.BlobAccess.Core/Model/IBlobContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.BlobAccess.Core.Model
+{
+    public interface IBlobContainer
+    {
+        string Name { get; set; }
+        DateTime LastModified { get; set; }
+    }
+}
---
+++ src/Azure.BlobAccess.Core/Repository/IBlobContainerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Azure.BlobAccess.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.BlobAccess.Core.Repository
+{
+    public interface IBlobContainerRepository
+    {
+        Task<IEnumerable<IBlobContainer>> GetAllBlobContainers();
+        Task<IBlobContainer> GetBlobContainerByName();
+        Task CreateNewBlobContainer(IBlobContainer blobContainer);
+        Task RenameBlobContainer(IBlobContainer blobContainer, string name);
+        Task DeleteBlobContainer(IBlobContainer blobContainer);
+    }
+}
